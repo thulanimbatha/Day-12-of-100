95d9d3ad07f0fb795a91b041f3b1533503ea00a5
thulanimbatha <thulanimbatha06@gmail.com>
2021-09-22 18:06:07 +0200

    completed step 1

--- num_guess.py
+++ num_guess.py
@@ -1,22 +1,14 @@
 # Day project - number guess game
-import random
+from random import randint
+# 1 - comp choose random number between 1 - 100
+comp_guess = randint(1, 100)
 
-def num_guess():
-    comp_guess = random.randint(0, 100) # number between 0 and 100
-    user_guess = int(input("Computer has generated a random number 0-100.\nCan you guess it?\nEnter guess: "))
-    end_of_game = False
-    number_of_guesses = 4
+# 2 - set difficulty
 
-    while not end_of_game and number_of_guesses >= 0:
-        print(f"\tYou have {number_of_guesses} guesses left.")
-        if user_guess < comp_guess:
-            number_of_guesses -= 1
-            user_guess = int(input("Too low. Try again: "))
-        elif user_guess > comp_guess:
-            number_of_guesses -= 1
-            user_guess = int(input("Too high. Try again: ")) 
-        elif user_guess == comp_guess:
-            print(f"You guessed correctly on {user_guess}")
-            end_of_game = True
- 
-num_guess()
+# 3 - let user guess number
+
+# 4 - check user guess
+
+# 5 - track number of guesses
+
+# 6 - repeat if user gets guess wrong
