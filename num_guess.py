b54496d561279281d09c4351f0c17d435e308199
thulanimbatha <thulanimbatha06@gmail.com>
2021-09-22 18:49:35 +0200

    completed steps 5 & 6

--- num_guess.py
+++ num_guess.py
@@ -15,23 +15,30 @@ def check_answer(user_guess, comp_guess):
 
 # 2 - set difficulty
 def set_difficulty():
-    level = input("Choose a difficulty. (type 'easy' or 'hard'").lower()
+    level = input("Choose a difficulty. (type 'easy' or 'hard'): ").lower()
     if level == "easy":
         return EASY_LEVEL_NUM
     else:
         return HARD_LEVEL_NUM
 
-# 1 - comp choose random number between 1 - 100
-print("Welcome to the Guessing Game! Can you beat the computer?")
-comp_guess = randint(1, 100)
-print("Computer chose a number between 1 and 100.")
+'''PROGRAM START'''
+def num_guess():
+    # 1 - comp choose random number between 1 - 100
+    print("Welcome to the Guessing Game! Can you beat the computer?")
+    comp_guess = randint(1, 100)
+    print("Computer chose a number between 1 and 100. See if you can guess it.")
 
+    # 3 - let user guess number
+    number_of_guesses = set_difficulty()
+    print(f"You have {number_of_guesses} attempts left.")
 
-# 3 - let user guess number
-user_guess = int(input("Make your guess: "))
-number_of_guesses = set_difficulty()
-print(f"You have {number_of_guesses} attempts left.")
+    user_guess = -1
 
-# 5 - track number of guesses
+    # 6 - repeat if user gets guess wrong
+    while user_guess != comp_guess:
+        user_guess = int(input("Make your guess: "))
 
-# 6 - repeat if user gets guess wrong
+        # 5 - track number of guesses
+        check_answer(user_guess, comp_guess)
+
+num_guess()
