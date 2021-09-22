9fea5d259af7aa09828e665a65e3cf092c1a4f1e
thulanimbatha <thulanimbatha06@gmail.com>
2021-09-22 13:43:39 +0200

    added version 2.0

--- num_guess.py
+++ num_guess.py
@@ -3,10 +3,14 @@ import random
 
 comp_guess = random.randint(0, 100) # number between 0 and 100
 user_guess = int(input("Computer has generated a random number 0-100.\nCan you guess it?\nEnter guess: "))
+end_of_game = False
+# number_of_guesses = 10
 
-if user_guess < comp_guess:
-    print("Too low")
-elif user_guess > comp_guess:
-    print("Too high")
-else:
-    print(f"You guessed correctly on {user_guess}")
+while user_guess != comp_guess or not end_of_game:
+    if user_guess < comp_guess:
+        user_guess = int(input("Too low. Try again: "))
+    elif user_guess > comp_guess:
+        user_guess = int(input("Too high. Try again: "))
+    else:
+        print(f"You guessed correctly on {user_guess}")
+        end_of_game = True
